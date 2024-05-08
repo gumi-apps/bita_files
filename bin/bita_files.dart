a0274ec3d55bf8ee80b2aa6df40112e476d0da3a
nurad <nuradhussen082@gmail.com>
2024-05-08 15:28:46 +0300

    project set up 101]

---
+++ bin/bita_files.dart
@@ -0,0 +1,5 @@
+import 'package:bita_files/bita_files.dart' as bita_files;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bita_files.calculate()}!');
+}
---
+++ test/bita_files_test.dart
@@ -0,0 +1,8 @@
+import 'package:bita_files/bita_files.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
